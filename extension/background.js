f20e72d50a70e8fb7341f1fbf1c1150cb30d7f65
CaptSiro <honzahorak433@icloud.com>
2024-03-20 22:50:50 +0100

    Removed background service

--- extension/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onStartup.addListener(async () => {
-    const request = new URL("http://localhost/newtab/api/session.php");
-    request.searchParams.set("m", "r"); // mode: "reset"
-    request.searchParams.set("sid", String(performance.timeOrigin));
-
-    await fetch(request);
-});
